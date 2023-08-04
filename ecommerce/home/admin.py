e096ff0e9a3617a059cd2ab98edd878e82368fd1
parashuramkalakutagi <parashuramkalakutagi9@gmail.com>
2023-08-04 07:35:12 +0530

    updated new models

--- ecommerce/home/admin.py
+++ ecommerce/home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import *
-
 admin.site.register(item)
 admin.site.register(orderitem)
 admin.site.register(order)
--- ecommerce/home/models.py
+++ ecommerce/home/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from accounts.models import User
+import uuid
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    class Meta:
+        abstract  = True
 
 CATAGORY_CHOICES = (
     ('s','shirt'),
@@ -15,7 +24,7 @@ LABALE_CHOICES = (
 
 
 
-class item(models.Model):
+class item(BaseModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     price = models.FloatField(default=0,null=True,blank=True)
@@ -28,16 +37,16 @@ class item(models.Model):
     def __str__(self):
         return self.title
 
-class orderitem(models.Model):
+class orderitem(BaseModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    items = models.ForeignKey(item,on_delete=models.CASCADE)
+    items = models.ForeignKey(item,on_delete=models.CASCADE,related_name='orderitem',related_query_name='orderitem')
     quntity = models.IntegerField(default=1)
 
     def __str__(self):
         return self.item
-class order(models.Model):
+class order(BaseModel):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
-    items = models.ManyToManyField(orderitem)
+    items = models.ManyToManyField(orderitem,related_name='order',related_query_name='order')
     created_at = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
